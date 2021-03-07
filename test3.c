44fd1a18e4f7f42aa0e853c99af35b2ae54089ce
Mattis Lind <mattis@mattisbogen.se>
2021-03-07 22:06:30 +0100

    Fixed scaling and plotting small symbols instead of just dots.

---
+++ test3.c
@@ -0,0 +1,97 @@
+#include "printf.h"
+
+#define CONSOLE_RX_CS 0177560
+#define CONSOLE_RX_DB 0177562
+#define CONSOLE_TX_CS 0177564
+#define CONSOLE_TX_DB 0177566
+#define AR11_AD_CS    0170400
+#define AR11_AD_DB    0170402
+#define AR11_AD_CS_UNIPOLAR    1 << 13
+#define AR11_AD_CS_START       1 << 0
+#define AR11_AD_CS_DONE        1 << 7
+#define AR11_DP_CS    0170410
+#define AR11_DP_DX    0170412
+#define AR11_DP_DY    0170414
+#define AR11_DP_CS_INTENS      1 << 0
+#define AR11_DP_CS_DONE        1 << 7
+
+int putchar (int ch)
+{
+  while (*((volatile char *) (CONSOLE_TX_CS)) >= 0); // wait for TX ready
+  *((volatile int *) (CONSOLE_TX_DB)) = ch;
+  return 0;
+}
+
+void putch (void * p, char c) {
+  putchar (c);
+}
+
+
+int getAD (int channel) {
+  *((volatile int *) (AR11_AD_CS)) = ((channel & 0xf) << 8) | AR11_AD_CS_UNIPOLAR | AR11_AD_CS_START;
+  while (*((volatile char *) (AR11_AD_CS)) >= 0); // wait for AD Ready
+  return (*((volatile int *) (AR11_AD_DB)));
+}
+
+void plotXY (int x, int y) {
+  while (*((volatile char *) (AR11_DP_CS)) >= 0);
+  (*((volatile int *) (AR11_DP_DX))) = x;
+  (*((volatile int *) (AR11_DP_DY))) = y;
+  *((volatile int *) (AR11_DP_CS)) = AR11_DP_CS_INTENS;
+}
+
+
+int puts (const char * str) {
+  char ch;
+  while ((ch=*str++)) {
+    putchar(ch);
+  }
+}
+
+char getchar () {
+  while (*((volatile char *) (CONSOLE_RX_CS))>=0); // Wait for character received
+  return (*((volatile int *) (CONSOLE_RX_DB))) & 0x7f;
+}
+
+
+#define SIZE 10
+
+void plotSquare(int x, int y) {
+  plotXY(x-SIZE, y-SIZE);
+  plotXY(x+SIZE, y-SIZE);
+  plotXY(x-SIZE, y+SIZE);
+  plotXY(x+SIZE, y+SIZE);
+  plotXY(x, y-SIZE);
+  plotXY(x+SIZE, y);
+  plotXY(x, y+SIZE);
+  plotXY(x-SIZE, y);  
+}
+
+void plotStar(int x, int y) {
+
+  plotXY(x-SIZE,y);
+  plotXY(x,y);
+  plotXY(x+SIZE,y);
+  plotXY(x,y+SIZE);
+  plotXY(x,y);
+  plotXY(x,y-SIZE);
+}
+
+int main ()
+{
+  int x1,y1,x2,y2;
+  init_printf((void *) 0, putch);
+  while (1) {
+    x1=1024-((getAD(0)-300)<<1);
+    y1=(getAD(1)-300)<<1;
+    plotStar(x1,y1);
+    x2=1024-((getAD(2)-300)<<1);
+    y2=(getAD(3)-300)<<1;
+    plotSquare(x2,y2);
+  }
+  return 0;
+}
+
+
+
+
